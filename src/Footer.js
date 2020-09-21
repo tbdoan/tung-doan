013f052c78ee2d827eacfca1aab1569f9167d3b7
tbdoan <tbdoan@ucsd.edu>
2020-09-21 15:08:48 -0700

    changed scroll code to use functional components

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        {' '}
+        source <a href="https://github.com/tbdoan/home">here</a>{' '}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
