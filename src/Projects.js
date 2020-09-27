3825cc5bb3559e82d7636c28c0924162599db640
tbdoan <tbdoan@ucsd.edu>
2020-09-26 21:01:44 -0700

    holy shit

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,4 @@
 import React, { useState } from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
 const Projects = ({ id }) => {
   const [show, setShow] = useState(false);
@@ -16,60 +13,33 @@ const Projects = ({ id }) => {
       className="section"
       id={id}
     >
-      <Accordion className="container project-accordion">
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle
-              as={Button}
-              onClick={() => setShow(true)}
-              variant="link"
-              className="project-title"
-              eventKey="0"
-            >
-              <h5>DodgePOG</h5>
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>
-              <p
-                style={{ fontStyle: 'italic', color: 'rgba(255,255,255, 0.7)' }}
-              >
-                Technologies used: Electron, jQuery, Puppeteer, sci-kit learn,
-                Keras, Jupyter Notebook
-              </p>
-              Desktop app that uses a machine learning algorithm to predict win
-              rates in the loading screen of a League of Legends match. Source
-              code <a href="https://github.com/DhanviDesu/Dodgepog">here</a>.
-              <br />
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle
-              as={Button}
-              className="project-title"
-              variant="link"
-              eventKey="1"
-            >
-              <h5>Spotiqueue</h5>
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>
-              <p
-                style={{ fontStyle: 'italic', color: 'rgba(255,255,255, 0.7)' }}
-              >
-                Technologies used: Typescript, Node, Express, Firebase, Postman
-              </p>
-              SMS app that allows hosts to create a room and guests to add songs
-              to the host's queue by sending a short code followed by a song
-              link. Uses the Spotify and Twilio APIs. Source code{' '}
-              <a href="https://github.com/tbdoan/qmeup">here</a>.
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
+      <div class="accordion">
+        <section class="accordion-item">
+          <h1>Section 1</h1>
+          <div class="accordion-item-content">
+            <p>Content for section 1</p>
+          </div>
+        </section>
+        <section class="accordion-item">
+          <h1>Section 2</h1>
+          <div class="accordion-item-content">
+            <p>Content for section 2</p>
+          </div>
+        </section>
+        <section class="accordion-item accordion-item--default">
+          <h1>Section 3</h1>
+          <div class="accordion-item-content">
+            <p>Content for section 3</p>
+            <p>This is my default state.</p>
+          </div>
+        </section>
+        <section class="accordion-item">
+          <h1>Section 4</h1>
+          <div class="accordion-item-content">
+            <p>Content for section 4</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
